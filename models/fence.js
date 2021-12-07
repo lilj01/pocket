ce10c8216efd482d9ac4fe1a7f4498ae2a612b71
lilj <bclzxc@163.com>
2021-12-07 19:45:49 +0800

    cell对象去重

--- models/fence.js
+++ models/fence.js
@@ -7,14 +7,28 @@ class Fence {
   /**规格值 一行 */
   cells = []
   specs
+  title
+  id
 
   constructor(specs) {
     this.specs = specs
+    this.title = specs[0].key
+    this.id = specs[0].key_id
   }
 
   init() {
-    this.specs.forEach(element => {
-      const cell = new Cell(element)
+    this._initCells()
+  }
+
+  _initCells() {
+    this.specs.forEach(s => {
+      const existed = this.cells.some(c => {
+        return c.id === s.value_id
+      })
+      if (existed) {
+        return
+      }
+      const cell = new Cell(s)
       this.cells.push(cell)
     })
   }
